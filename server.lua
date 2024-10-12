aa3d3c1a4c82750b01c01890fc3e3e11730eb9e0
BibiModz <83229685+bibimodz@users.noreply.github.com>
2024-10-12 22:46:04 +0200

    Update server.lua

--- server.lua
+++ server.lua
@@ -1,11 +1,10 @@
-local ESX = exports['es_extended']:getSharedObject()
-
+ESX = exports['es_extended']:getSharedObject()
 
 local function sendToDiscordLog(message)
     local content = {
         {
-            ["color"] = 16711680, -- Rouge vif
-            ["title"] = "Dépeçage de PNJ",
+            ["color"] = 16711680, 
+            ["title"] = "Vente d'Items",
             ["description"] = message,
             ["footer"] = {
                 ["text"] = "Log du serveur FiveM"
@@ -22,40 +21,89 @@ end
 
 ESX.RegisterServerCallback('bibiModz:hasScalpel', function(source, cb)
     local xPlayer = ESX.GetPlayerFromId(source)
-    local scalpel = xPlayer.getInventoryItem('scalpel')
-
-    if scalpel and scalpel.count > 0 then
-        cb(true)
-    else
-        cb(false)
-    end
+    local scalpel = xPlayer.getInventoryItem(Config.ScalpelItem)
+    cb(scalpel and scalpel.count > 0)
 end)
 
-RegisterServerEvent('bibiModz:giveRandomItems')
-AddEventHandler('bibiModz:giveRandomItems', function(itemsToGive)
+
+RegisterNetEvent('bibiModz:giveRandomItems')
+AddEventHandler('bibiModz:giveRandomItems', function(items)
     local xPlayer = ESX.GetPlayerFromId(source)
-    local receivedItems = {}
-
-    for _, item in ipairs(itemsToGive) do
-        xPlayer.addInventoryItem(item.name, item.count)
-        table.insert(receivedItems, item.name .. " (x" .. item.count .. ")")
+    if xPlayer then
+        for _, item in pairs(items) do
+            xPlayer.addInventoryItem(item, 1)
+        end
     end
-
-    local itemsString = table.concat(receivedItems, ", ")
-    local playerName = GetPlayerName(source)
-    local weaponLabel = "Inconnue"
-    sendToDiscordLog("Le joueur **" .. playerName .. "** a dépecé un PNJ et a reçu les items suivants : **" .. itemsString .. "**.")
 end)
 
-RegisterServerEvent('bibiModz:buyScalpel')
+RegisterNetEvent('bibiModz:buyScalpel')
 AddEventHandler('bibiModz:buyScalpel', function()
     local xPlayer = ESX.GetPlayerFromId(source)
-    local price = Config.ScalpelPrice
-    if xPlayer.getMoney() >= price then
-        xPlayer.removeMoney(price)
-        xPlayer.addInventoryItem('scalpel', 1)
-        TriggerClientEvent('esx:showNotification', source, 'Vous avez acheté un scalpel pour $' .. price .. '.')
+    local scalpelPrice = Config.ScalpelPrice
+
+    if xPlayer.getMoney() >= scalpelPrice then
+        xPlayer.removeMoney(scalpelPrice)
+        xPlayer.addInventoryItem(Config.ScalpelItem, 1)
+        TriggerClientEvent('esx:showNotification', source, 'Vous avez acheté un scalpel pour $' .. scalpelPrice)
     else
         TriggerClientEvent('esx:showNotification', source, 'Vous n\'avez pas assez d\'argent.')
+    end
+end)
+
+RegisterNetEvent('bibiModz:sellAllItems')
+AddEventHandler('bibiModz:sellAllItems', function()
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local totalPrice = 0
+    local hasEnoughItems = false
+    local itemsSold = {}
+
+    for itemName, itemConfig in pairs(Config.ItemDetails) do
+        local item = xPlayer.getInventoryItem(itemName)
+        if item.count >= itemConfig.limit then
+            totalPrice = totalPrice + (item.count * itemConfig.price)
+            hasEnoughItems = true
+            xPlayer.removeInventoryItem(itemName, item.count) -- Retirer les items de l'inventaire
+            table.insert(itemsSold, itemName .. " (x" .. item.count .. ")")
+        end
+    end
+
+    if not hasEnoughItems then
+        TriggerClientEvent('esx:showNotification', source, 'Vous n\'avez pas assez d\'items pour les vendre.')
+        return
+    end
+
+    if totalPrice > 0 then
+        xPlayer.addMoney(totalPrice)
+        TriggerClientEvent('esx:showNotification', source, 'Vous avez vendu vos items pour $' .. totalPrice)
+
+
+        local itemsSoldString = table.concat(itemsSold, ", ")
+        sendToDiscordLog("Le joueur **" .. GetPlayerName(source) .. "** a vendu les items suivants : **" .. itemsSoldString .. "** pour un total de **" .. totalPrice .. "$**.")
+    else
+        TriggerClientEvent('esx:showNotification', source, 'Vous n\'avez pas d\'items à vendre.')
+    end
+end)
+
+RegisterNetEvent('bibiModz:sellSpecificItem')
+AddEventHandler('bibiModz:sellSpecificItem', function(itemName)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local itemConfig = Config.ItemDetails[itemName]
+
+    if not itemConfig then
+        TriggerClientEvent('esx:showNotification', source, 'Cet item ne peut pas être vendu.')
+        return
+    end
+
+    local item = xPlayer.getInventoryItem(itemName)
+
+    if item.count >= itemConfig.limit then
+        local totalPrice = item.count * itemConfig.price
+        xPlayer.removeInventoryItem(itemName, item.count)
+        xPlayer.addMoney(totalPrice)
+        TriggerClientEvent('esx:showNotification', source, 'Vous avez vendu tous les ' .. itemName .. ' pour $' .. totalPrice)
+
+        sendToDiscordLog("Le joueur **" .. GetPlayerName(source) .. "** a vendu **" .. item.count .. " " .. itemName .. "** pour **" .. totalPrice .. "$**.")
+    else
+        TriggerClientEvent('esx:showNotification', source, 'Vous devez avoir au moins ' .. itemConfig.limit .. ' ' .. itemName .. ' pour les vendre.')
     end
 end)
